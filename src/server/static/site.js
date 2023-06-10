608a82b15b91ee0929b1493f19749bbc78715191
JaktensTid <jaktenstid1@gmail.com>
2023-06-10 20:02:06 +0300

    feat(website): Changed pictures and added progress bar

--- src/server/static/site.js
+++ src/server/static/site.js
@@ -28,121 +28,31 @@ var localisation = function (probabilities) {
             mithochondria: {
                 image: _createImage('./static/images/mithochondria.png'),
                 controlElementId: 'mithochondria-list-item',
-                text: 'Mithochondria',
-                polygon: [
-                    [
-                        [150, 115],
-                        [182, 112],
-                        [217, 139],
-                        [210, 155],
-                        [180, 143],
-                        [156, 149]
-                    ],
-                    [
-                        [213, 352],
-                        [234, 345],
-                        [250, 353],
-                        [242, 385],
-                        [200, 390],
-                        [182, 377],
-                        [178, 353],
-                        [192, 345],
-                    ]
-                ]
+                text: 'Mithochondria'
             },
             nucleus: {
                 image: _createImage('./static/images/nucleus.png'),
                 controlElementId: 'nucleus-list-item',
-                text: 'Nucleus',
-                polygon: [
-                    [[176, 178],
-                        [202, 199],
-                        [222, 229],
-                        [216, 257],
-                        [170, 271],
-                        [134, 255],
-                        [137, 201],
-                        [169, 177]]
-                ]
+                text: 'Nucleus'
             },
             cytoplasm: {
                 image: _createImage('./static/images/cytoplasm.png'),
                 controlElementId: 'cytoplasm-list-item',
-                text: 'Cytoplasm',
-                polygon: [
-                    [
-                        [95, 160],
-                        [190, 99],
-                        [238, 167],
-                        [341, 203],
-                        [400, 313],
-                        [332, 385],
-                        [160, 381],
-                        [88, 315],
-                        [82, 201],
-                        [130, 107]
-                    ]
-                ]
+                text: 'Cytoplasm'
             },
             other: {
                 image: _createImage('./static/images/original.png'),
                 controlElementId: 'other-proteins-item',
-                text: 'Other proteins',
-                polygon: [
-                    [
-                        [0, 0],
-                        [500, 0],
-                        [500, 500],
-                        [0, 500]
-                    ]
-                ]
+                text: 'Other proteins'
             },
             extracellular: {
                 image: _createImage('./static/images/original.png'),
                 controlElementId: 'extracellular-proteins-item',
-                text: 'Extracellular proteins',
-                polygon: [
-                    [
-                        [0, 0],
-                        [500, 0],
-                        [500, 500],
-                        [0, 500]
-                    ]
-                ]
+                text: 'Extracellular proteins'
             },
             original: {
-                image: _createImage('./static/images/original.png'),
-                polygon: [
-                    [
-                        [0, 0],
-                        [500, 0],
-                        [500, 500],
-                        [0, 500]
-                    ]
-                ]
+                image: _createImage('./static/images/original.png')
             },
-        },
-        isInside: function inside(point, vsArray) {
-            for (const vs of vsArray) {
-                // ray-casting algorithm based on
-                // https://wrf.ecse.rpi.edu/Research/Short_Notes/pnpoly.html
-
-                const x = point[0], y = point[1];
-
-                let inside = false;
-                for (let i = 0, j = vs.length - 1; i < vs.length; j = i++) {
-                    let xi = vs[i][0], yi = vs[i][1];
-                    let xj = vs[j][0], yj = vs[j][1];
-
-                    let intersect = ((yi > y) != (yj > y))
-                        && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
-                    if (intersect) inside = !inside;
-                }
-
-                if (inside)
-                    return true;
-            }
-            return false;
         },
         render: function () {
             const highlightElements = [];
@@ -160,28 +70,6 @@ var localisation = function (probabilities) {
             }
 
             img.src = obj.highlightData.original.image.src;
-            img.addEventListener('mousemove', event => {
-                const rect = img.getBoundingClientRect();
-                const x = event.clientX - rect.left;
-                const y = event.clientY - rect.top;
-                for (const key in obj.highlightData) {
-                    const data = obj.highlightData[key];
-                    if (obj.isInside([x, y], data.polygon)) {
-                        for (const highlightElement of highlightElements) {
-                            highlightElement.removeClass('active');
-                        }
-                        $('#' + data.controlElementId).addClass('active');
-                        img.src = data.image.src;
-                        return;
-                    }
-                }
-            });
-            img.addEventListener('mouseout', function () {
-                img.src = obj.highlightData.original.image.src;
-                for (const highlightElement of highlightElements) {
-                    highlightElement.removeClass('active');
-                }
-            });
         }
     }
     return obj;
@@ -228,5 +116,12 @@ var folding = function (proteinFileContent) {
         }
     }
     return obj;
+}
+
+var spinner = function() {
+    $('#inferenceInputForm').on('submit', function() {
+        $('#submitInference').attr('disabled', true);
+        $('#spinner').removeClass('invisible-spinner');
+    })
 }
 
