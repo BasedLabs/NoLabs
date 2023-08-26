9d5dd902c2ec0fa6cd10ee27f0bb085f7e5814e6
JaktensTid <jaktenstid1@gmail.com>
2023-08-26 21:33:32 +0300

    feat(obo): Added a baseline tests for gene ontology

--- src/server/run.py
+++ src/server/run.py
@@ -5,7 +5,6 @@ import torch
 from flask import Flask, render_template, request
 import src.server.services.inference_service as inference_service
 from server.services.oboreader import read_obo
-from server.services.obo_loader_service import load_obo
 from src.server.services.fasta_reader import get_sequences
 
 app = Flask(__name__)
@@ -23,36 +22,23 @@ def hello():
     return render_template('index.html')
 
 
-@app.route('/obo-graph-data')
+@app.route('/obo-graph-data', methods=['POST'])
 def cytoscape_data():
-    obo_graph, obo_graph_light = read_obo()
-    return {
-        'obo_graph': obo_graph,
-        'obo_graph_light': obo_graph_light
-    }
-
-@app.route('/cytoscape')
-def cytoscape():
-    return render_template('cytoscape.html')
-
-@app.route('/d3')
-def d3():
-    return render_template('d3.html')
+    mgnifyId = request.form['mgnifyId']
+    obo_graph = read_obo()
+    return obo_graph
 
 
 @app.route('/inference', methods=['POST'])
 def inference():
     amino_acid_input_sequence = request.form['inputSequence']
     amino_acid_input_sequence_files = request.files.getlist('inputSequenceFile')
-    if not amino_acid_input_sequence and not amino_acid_input_sequence_files:
-        return render_template('error.html', error='You must enter sequence or select a FASTA file')
     if not amino_acid_input_sequence:
         amino_acid_input_sequence = \
             [seq for seq in get_sequences(amino_acid_input_sequence_files)][0]
     gpu = request.form.get('gpu', False)
     if gpu and not torch.cuda.is_available():
         return render_template('error.html', error="You don't have a CUDA installed or NVIDIA videocard")
-    # aminoAcidInputSequenceFile = request.files['inputSequenceFile'] # will consider this later
     pipeline = inference_service.create_pipeline(use_gpu=gpu, is_test=is_test)
     localisation_result = inference_service.get_localisation_output(pipeline=pipeline,
                                                                     amino_acid_sequence=amino_acid_input_sequence)
@@ -62,21 +48,7 @@ def inference():
 
     esm_protein_localization = {key: value for key, value in localisation_result}
 
-    obo_ids = {
-        "GO:0009274",
-        "GO:0000724",
-        "GO:0009314",
-        "GO:0009432",
-        "GO:0005524",
-        "GO:0006281",
-        "GO:0006310",
-        "GO:0005524",
-        "GO:0006281",
-        "GO:0006974",
-        "GO:0000166"
-    }
-
-    inference_data = json.dumps({
+    return {
         'sequence': amino_acid_input_sequence,
         'localisation': {
             'mithochondria': esm_protein_localization["Mitochondrial Proteins"],
@@ -85,10 +57,8 @@ def inference():
             'other': esm_protein_localization["Other Proteins"],
             'extracellular': esm_protein_localization["Extracellular/Secreted Proteins"]
         },
-        'folding': folding_result,
-        'gene_ontology': load_obo(ids=obo_ids)
-    })
-    return render_template('index.html', inference=inference_data)
+        'folding': folding_result
+    }
 
 
 if __name__ == '__main__':
--- src/server/services/oboreader.py
+++ src/server/services/oboreader.py
@@ -8,17 +8,17 @@ def read_obo():
     graph = obonet.read_obo(url)
 
     ids = {
-    "GO:0009274",
-    "GO:0000724",
-    "GO:0009314",
-    "GO:0009432",
-    "GO:0005524",
-    "GO:0006281",
-    "GO:0006310",
-    "GO:0005524",
-    "GO:0006281",
-    "GO:0006974",
-    "GO:0000166"
+        "GO:0009274",
+        "GO:0000724",
+        "GO:0009314",
+        "GO:0009432",
+        "GO:0005524",
+        "GO:0006281",
+        "GO:0006310",
+        "GO:0005524",
+        "GO:0006281",
+        "GO:0006974",
+        "GO:0000166"
     }
 
     def get_attributes(edge_data):
@@ -26,15 +26,6 @@ def read_obo():
             if isinstance(key, numbers.Number):
                 return get_attributes(edge_data[key])
             return [k for k in edge_data.keys()]
-
-    lightened_graph = {} # {nodeId: {name: '', edges: { nodeId: {linkType}, }}}
-
-    for node in graph.nodes:
-        if node not in lightened_graph:
-            lightened_graph[node] = {'name': graph.nodes[node]['name'], 'edges': {}}
-        for out_edge in graph.out_edges(node):
-            out_node = out_edge[1]
-            lightened_graph[node]['edges'][out_node] = get_attributes(graph.get_edge_data(node, out_edge[1]))
 
     # cleanup a graph from the nodes that we don't have in list
     nodes_to_remove = []
@@ -53,13 +44,18 @@ def read_obo():
     for node in nodes_to_remove:
         graph.remove_node(node)
 
-    shaped_graph = {}
+    shaped_graph = {} # {nodeId: {name: '', edges: { nodeId: {linkType}, }}}
     for node in graph.nodes:
         if node not in shaped_graph:
-            shaped_graph[node] = {'name': graph.nodes[node]['name'], 'edges': {}}
+            shaped_graph[node] = {'name': graph.nodes[node]['name'], 'namespace': graph.nodes[node]['namespace'],
+                                  'edges': {}}
         for out_edge in graph.out_edges(node):
             out_node = out_edge[1]
             edge_data = get_attributes(graph.get_edge_data(node, out_edge[1]))
             shaped_graph[node]['edges'][out_node] = edge_data
 
-    return (lightened_graph, shaped_graph)
+    return shaped_graph
+
+
+if __name__ == "__main__":
+    obo = read_obo()
