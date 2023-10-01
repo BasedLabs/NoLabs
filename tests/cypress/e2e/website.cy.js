84e839d44fabbf3f6e523b088cb92a6f37fbb067
JaktensTid <jaktenstid1@gmail.com>
2023-10-02 01:05:40 +0300

    fix(cypress): Fixed cypress

--- tests/cypress/e2e/website.cy.js
+++ tests/cypress/e2e/website.cy.js
@@ -1,24 +1,44 @@
 describe('Main page', () => {
-  const inputAminoAcidSequenceId = '#inputSequence';
-  const submitAminoAcidSequenceId = '#submitInference';
-  const localisationImageClass = '.localisation-image';
-  const foldingSelector = '#viewport div';
+    const inputPdbId = '#proteinFileInput';
+    const inputAminoAcidSequenceId = '#inputSequence';
+    const submitAminoAcidSequenceId = '#submitInference';
+    const localisationImageClass = '.localisation-image';
+    const foldingSelector = '#viewport div';
 
-  it('Opened main page', () => {
-    cy.visit('http://127.0.0.1:5000');
-  });
+    it('Opened main page', () => {
+        cy.visit('http://127.0.0.1:5000');
+    });
 
-  it('Amino-acid sequence input is visible', () => {
-    cy.visit('http://127.0.0.1:5000');
-    cy.get(inputAminoAcidSequenceId).should('exist');
-  });
+    it('Amino Acid lab opened', () => {
+        cy.visit('http://127.0.0.1:5000/amino-acid-page');
+    });
 
-  it('Inference result is visible', () => {
-    cy.visit('http://127.0.0.1:5000');
-    cy.get(inputAminoAcidSequenceId).click();
-    cy.get(inputAminoAcidSequenceId).type(`AAACGAGGCAA`);
-    cy.get(submitAminoAcidSequenceId).click();
-    cy.get(localisationImageClass).should('exist');
-    cy.get(foldingSelector, { timeout: 90000 }).should('exist');
-  });
+    it('Amino-acid sequence input is visible', () => {
+        cy.visit('http://127.0.0.1:5000/amino-acid-page');
+        cy.get(inputAminoAcidSequenceId).should('exist');
+    });
+
+    it('Inference result is visible', () => {
+        cy.visit('http://127.0.0.1:5000/amino-acid-page');
+        cy.get(inputAminoAcidSequenceId).click();
+        cy.get(inputAminoAcidSequenceId).type(`AAACGAGGCAA`);
+        cy.get(submitAminoAcidSequenceId).click();
+        cy.get(localisationImageClass).should('exist');
+        cy.get(foldingSelector, {timeout: 90000}).should('exist');
+    });
+
+    //it('Drug target discovery lab opened', () => {
+    //    cy.visit('http://127.0.0.1:5000/drug-target-discovery');
+    //});
+//
+    //it('Pdb input is visible', () => {
+    //    cy.visit('http://127.0.0.1:5000');
+    //});
+//
+    //it('Inference result is visible', () => {
+    //    cy.get('input[id=proteinFileInput]').selectFile('cypress/fixtures/test.pdb');
+    //    cy.get('input[id=smilesFileInput]').selectFile('cypress/fixtures/test.sdf');
+    //    cy.get('#submitInference').click();
+    //    cy.get('#drugTargetDiscovery', {timeout: 90000}).should('exist');
+    //});
 })
