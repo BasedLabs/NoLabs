66ccef8fd65ad7f5d80ee046910062458d723af9
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2024-01-29 23:45:48 +0000

    Fixed a bug with type of is_running for umol client

--- microservices/umol/umol/api_models.py
+++ microservices/umol/umol/api_models.py
@@ -25,7 +25,7 @@ class RunUmolPredictionResponse(BaseModelMixin, ErrorResponseMixin):
     pdb_contents: Optional[str] = None
     plddt_array: List[int] = dataclasses.field(default_factory=list)
 
-
+@pydantic.dataclasses.dataclass
 @dataclasses.dataclass
 class IsJobRunningResponse:
     is_running: bool
