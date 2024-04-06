282c655bc4c12c421a6aee139f04e8a008deac66
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2024-04-06 18:23:37 +0100

    Planning + returns function calls with langchain

--- microservices/biobuddy/biobuddy/services.py
+++ microservices/biobuddy/biobuddy/services.py
@@ -1,4 +1,7 @@
+from langchain.tools.render import render_text_description
+from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.tools import BaseTool
 
 from biobuddy.api_models import SendMessageToBioBuddyResponse, SendMessageToBioBuddyRequest
 from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
@@ -14,9 +17,8 @@ def send_message(request: SendMessageToBioBuddyRequest) -> SendMessageToBioBuddy
         "You are a biobuddy. You are a research assistant that helps creating new drugs, researching biology, "
         "pulling information from different sources and running experiments. "
         "If a user asks you to pull the targets only do so for proteins and use rcsb pdb. If a user asks for "
-        "drugs/ligands then use chembl. If a user asks for latest research about the illness then use pubmed."
+        "drugs/ligands then use chembl."
     )
-
     history_messages = [SystemMessage(content=system_message_content)]
     for msg in request.previous_messages:
         if 'user' in msg.keys():
@@ -30,16 +32,47 @@ def send_message(request: SendMessageToBioBuddyRequest) -> SendMessageToBioBuddy
     ])
 
     runnable = prompt_template | chat_model
-    tools_description = " ".join([f"{tool}, " for tool in request.tools])
+
+    tools_description = " ".join([f"{(tool['function']['name'], tool['function']['description'])}, " for tool in request.tools])
+
+    openai_functions = [tool['function'] for tool in request.tools]
 
     strategy_prompt = (f"Given the available tools ({tools_description}), decide whether a direct reply is sufficient "
-                       f"or if a specific plan involving these tools is necessary. Then, either provide the direct "
-                       f"reply or outline the plan with function calls.\n\nQuery: \""
-                       f"{request.message_content}\"\n\nDecision:")
+                       f"or if a specific plan involving these tools is necessary. If plan is not needed, provide the direct "
+                       f"reply. If plan is needed, reply with the list (with a tag <PLAN> before the list) of function calls in the format <PLAN>: ['Call function_1 in order to do X', 'Call function_2 to achieve Y'].\n\nQuery: \""
+                       f"{request.message_content}\"\n\n")
+
+
+    tool_prompt = f"""You are an assistant that has access to the following set of tools. Here are the names and descriptions for each tool:
+
+    {request.tools}
+
+    Given the user input, return the name and input of the tool to use. Return your response as a JSON blob with 'name' and 'arguments' keys."""
+
+    prompt = ChatPromptTemplate.from_messages(
+        [("system", tool_prompt),
+         ("human", "{input}")]
+    )
+
+    tool_chain = prompt | chat_model | JsonOutputParser()
 
     completion = runnable.invoke({
         "input": strategy_prompt,
         "history": history_messages
     })
 
-    return SendMessageToBioBuddyResponse(chatgpt_reply=completion.content)
+    if "<PLAN>" in completion.content:
+        plan_text = completion.content.split("<PLAN>:")[1].strip()
+        plan_actions = eval(plan_text)
+
+        function_calls = []
+        for action in plan_actions:
+            print(action)
+            action_completion = chat_model.invoke( [HumanMessage(content=action)], functions=openai_functions)
+            function_calls.append(str(action_completion))
+
+        response_content = "\n".join(function_calls)
+    else:
+        response_content = completion.content
+
+    return SendMessageToBioBuddyResponse(chatgpt_reply=response_content)
