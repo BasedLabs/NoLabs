30e2162ad7beb14b1588be695507018fc8928a47
JaktensTid <jaktenstid1@gmail.com>
2024-04-27 18:32:16 +0300

    feat: github ci/cd

--- .github/scripts/parse_compose_service_build_context.py
+++ .github/scripts/parse_compose_service_build_context.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import yaml
 from pathlib import Path
@@ -5,7 +6,9 @@ from pathlib import Path
 
 def parse_compose_service_build_context(microservice_name):
     compose = yaml.safe_load(Path('compose.yaml').read_text())
-    print(compose['services'][microservice_name]['build']['context'])
+    build_context = compose['services'][microservice_name]['build']['context']
+    with open(os.environ['BUILD_CONTEXT'], 'a') as fh:
+        print(f'name={build_context}', file=fh)
 
 
 if __name__ == '__main__':
--- .github/scripts/parse_compose_service_image.py
+++ .github/scripts/parse_compose_service_image.py
@@ -1,3 +1,4 @@
+import os
 import sys
 import yaml
 from pathlib import Path
@@ -5,7 +6,9 @@ from pathlib import Path
 
 def parse_compose_service_image(microservice_name):
     compose = yaml.safe_load(Path('compose.yaml').read_text())
-    print(compose['services'][microservice_name]['image'])
+    image = compose['services'][microservice_name]['image']
+    with open(os.environ['IMAGE_TAG'], 'a') as fh:
+        print(f'name={image}', file=fh)
 
 
 if __name__ == '__main__':
