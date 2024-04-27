a0370d79fa82ba1fd7b450354443ab65abe4f57e
JaktensTid <jaktenstid1@gmail.com>
2024-04-27 18:41:49 +0300

    feat: github ci/cd

--- .github/scripts/parse_compose_service_build_context.py
+++ .github/scripts/parse_compose_service_build_context.py
@@ -7,8 +7,9 @@ from pathlib import Path
 def parse_compose_service_build_context(microservice_name):
     compose = yaml.safe_load(Path('compose.yaml').read_text())
     build_context = compose['services'][microservice_name]['build']['context']
-    with open(os.environ['BUILD_CONTEXT'], 'a') as fh:
-        print(f'name={build_context}', file=fh)
+    env_file = os.getenv('GITHUB_ENV')
+    with open(env_file, 'a') as fh:
+        print(f'BUILD_CONTEXT={build_context}', file=fh)
 
 
 if __name__ == '__main__':
--- .github/scripts/parse_compose_service_image.py
+++ .github/scripts/parse_compose_service_image.py
@@ -7,8 +7,9 @@ from pathlib import Path
 def parse_compose_service_image(microservice_name):
     compose = yaml.safe_load(Path('compose.yaml').read_text())
     image = compose['services'][microservice_name]['image']
-    with open(os.environ['IMAGE_TAG'], 'a') as fh:
-        print(f'name={image}', file=fh)
+    env_file = os.getenv('GITHUB_ENV')
+    with open(env_file, 'a') as fh:
+        print(f'IMAGE_TAG={image}', file=fh)
 
 
 if __name__ == '__main__':
