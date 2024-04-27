313d1665e1c8d0d2bc764b2132311b05c58365dd
JaktensTid <jaktenstid1@gmail.com>
2024-04-27 18:24:25 +0300

    feat: github ci/cd

--- .github/scripts/parse_compose_service_build_context.py
+++ .github/scripts/parse_compose_service_build_context.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def parse_compose_service_build_context(microservice_name):
     compose = yaml.safe_load(Path('compose.yaml').read_text())
-    return compose['services'][microservice_name]['build']['context']
+    print(compose['services'][microservice_name]['build']['context'])
 
 
 if __name__ == '__main__':
--- .github/scripts/parse_compose_service_image.py
+++ .github/scripts/parse_compose_service_image.py
@@ -5,7 +5,7 @@ from pathlib import Path
 
 def parse_compose_service_image(microservice_name):
     compose = yaml.safe_load(Path('compose.yaml').read_text())
-    return compose['services'][microservice_name]['image']
+    print(compose['services'][microservice_name]['image'])
 
 
 if __name__ == '__main__':
