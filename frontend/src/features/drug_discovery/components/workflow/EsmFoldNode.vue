f767aecd07d7d1b548b8466845071723bed74aeb
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2024-06-01 12:30:14 +0100

    Moved intputs and outputs to separate component

--- frontend/src/features/drug_discovery/components/workflow/EsmFoldNode.vue
+++ frontend/src/features/drug_discovery/components/workflow/EsmFoldNode.vue
@@ -9,33 +9,21 @@
     <EsmFoldNodeContent :results="results" />
     <q-btn @click="openDialogue" class="full-width" icon="open_in_new" label="Extended view"> </q-btn>
   </q-card>
-  <div class="q-mt-md">
-      <q-item-label class="text-bold">Inputs:</q-item-label>
-      <div v-for="(input, index) in inputs" :key="'input-' + index" class="row no-wrap items-center q-my-xs">
-        <Handle type="target" :position="Position.Left" :id="`${nodeId}-input-${input}`" :style="{ position: 'relative', left: '50%', zIndex: 10 }" />
-        <q-item-label class="q-ml-xs">{{ input }}</q-item-label>
-      </div>
-    </div>
-
-    <div class="q-mt-md">
-      <q-item-label class="text-bold">Outputs:</q-item-label>
-      <div v-for="(output, index) in outputs" :key="'output-' + index" class="row no-wrap items-center q-my-xs">
-        <q-item-label class="q-mr-xs">{{ output }}</q-item-label>
-        <Handle type="source" :position="Position.Right" :id="`${nodeId}-output-${output}`" :style="{ position: 'relative', left: '50%', zIndex: 10 }" />
-      </div>
-    </div>
+  
+  <NodeHandles :nodeId="nodeId" :inputs="inputs" :outputs="outputs" />
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import { Handle, Position } from "@vue-flow/core";
+import { Position } from "@vue-flow/core";
 import EsmFoldNodeContent from './EsmFoldNodeContent.vue';
+import NodeHandles from './nodeTemplates/NodeHandles.vue'
 
 export default defineComponent({
   name: 'EsmFoldNode',
   components: {
     EsmFoldNodeContent,
-    Handle
+    NodeHandles
   },
   props: {
     nodeId: String,
---
+++ frontend/src/features/drug_discovery/components/workflow/nodeTemplates/NodeHandles.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="q-mt-md">
+      <q-item-label class="text-bold">Inputs:</q-item-label>
+      <div v-for="(input, index) in inputs" :key="'input-' + index" class="row no-wrap items-center q-my-xs">
+        <Handle type="target" :position="Position.Left" :id="`${nodeId}-input-${input}`" :style="{ position: 'relative', left: '50%', zIndex: 10 }" />
+        <q-item-label class="q-ml-xs">{{ input }}</q-item-label>
+      </div>
+  
+      <q-item-label class="text-bold">Outputs:</q-item-label>
+      <div v-for="(output, index) in outputs" :key="'output-' + index" class="row no-wrap items-center q-my-xs">
+        <q-item-label class="q-mr-xs">{{ output }}</q-item-label>
+        <Handle type="source" :position="Position.Right" :id="`${nodeId}-output-${output}`" :style="{ position: 'relative', left: '50%', zIndex: 10 }" />
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { defineComponent } from 'vue';
+  import { Handle, Position } from "@vue-flow/core";
+  
+  export default defineComponent({
+    name: 'NodeHandles',
+    components: {
+      Handle
+    },
+    props: {
+      nodeId: {
+        type: String,
+        required: true
+      },
+      inputs: {
+        type: Array,
+        default: () => []
+      },
+      outputs: {
+        type: Array,
+        default: () => []
+      }
+    },
+    computed: {
+      Position() {
+        return Position;
+      }
+    }
+  });
+  </script>
+  
+  <style scoped>
+  .q-my-xs {
+    margin-top: 4px;
+    margin-bottom: 4px;
+  }
+  </style>
+  
