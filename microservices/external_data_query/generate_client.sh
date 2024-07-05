ead83ad17124b15a1cdf129e4c01866c39c671d0
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2024-07-05 19:37:30 +0100

    Fixed .sh file

--- microservices/external_data_query/generate_client.sh
+++ microservices/external_data_query/generate_client.sh
@@ -16,18 +16,18 @@ fi
 
 echo 'Installation: https://openapi-generator.tech/docs/installation'
 
-DOCKER_IMAGE_NAME="rcsb_pdb_query"
+DOCKER_IMAGE_NAME="external_data_query"
 
 sudo docker build -t $DOCKER_IMAGE_NAME -f build/Dockerfile .
 # Run the Docker container in the background
 # Replace the Docker image name with the appropriate one
-sudo docker run -d --name rcsb_pdb_query -p 5739:5739 rcsb_pdb_query --host=0.0.0.0 --port=5739
+sudo docker run -d --name external_data_query -p 5739:5739 external_data_query --host=0.0.0.0 --port=5739
 
 # Generate the Python client using OpenAPI Generator
 npx @openapitools/openapi-generator-cli generate \
     -i http://127.0.0.1:5739/openapi.json \
     -g python \
     -o ./client \
-    --additional-properties=packageName=rcsb_pdb_query_microservice
+    --additional-properties=packageName=external_data_query_microservice
 
 echo 'Use pip install ./client'
