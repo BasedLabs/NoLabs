ee4770a5cfc5d9dcb31997136c5c875a63fcc344
Timur Ishmuratov <ishmuratov7timur7@gmail.com>
2024-07-30 00:46:53 +0100

    Adjusted backend for blast workflow

--- nolabs/application/use_cases/blast/workflow.py
+++ nolabs/application/use_cases/blast/workflow.py
@@ -3,45 +3,51 @@ from typing import List, Type
 
 from pydantic import BaseModel
 
-from nolabs.application.use_cases.diffdock.api_models import SetupJobRequest
-from nolabs.application.use_cases.diffdock.use_cases import SetupJobFeature, RunJobFeature, GetJobFeature
-from nolabs.domain.models.common import Protein, Ligand
+from nolabs.application.use_cases.blast.api_models import SetupJobRequest
+from nolabs.application.use_cases.blast.use_cases import SetupJobFeature, RunJobFeature, GetJobFeature
+from nolabs.domain.models.common import Protein
 from nolabs.domain.models.blast import BlastJob
 from nolabs.infrastructure.di import InfrastructureDependencies
 from nolabs.application.workflow.component import Component, JobValidationError
 
 
-class DiffDockComponentInput(BaseModel):
-    proteins_with_pdb: List[uuid.UUID]
+class BlastComponentInput(BaseModel):
+    proteins_with_fasta: List[uuid.UUID]
+    descriptions: int = 10,
+    alignments: int = 10,
+    hitlist_size: int = 10,
+    expect: float = 10.0
 
 
-class DiffDockComponentOutput(BaseModel):
-    protein_complexes: List[uuid.UUID]
+
+class BlastComponentOutput(BaseModel):
+    hits: List[str]
 
 
-class DiffDockComponent(Component[DiffDockComponentInput, DiffDockComponentOutput]):
+class BlastComponent(Component[BlastComponentInput, BlastComponentOutput]):
     name = 'Blast'
     description = 'Finds similar sequences for proteins and nucleotides.'
 
     async def execute(self):
-        run_job_feature = RunJobFeature(diffdock=InfrastructureDependencies.diffdock_microservice())
+        run_job_feature = RunJobFeature(blast=InfrastructureDependencies.blast_query_microservice())
         get_job_feature = GetJobFeature()
 
         for job in self.jobs:
             await run_job_feature.handle(job_id=job.id)
 
-        items: List[uuid.UUID] = []
+        items: List[str] = []
 
         for job in self.jobs:
             get_result = await get_job_feature.handle(job_id=job.id)
 
             for result in get_result.result:
-                items.append(
-                    result.complex_id
-                )
+                for hit in result.hits:
+                    items.append(
+                        hit.id
+                    )
 
-        self.output = DiffDockComponentOutput(
-            protein_complexes=items
+        self.output = BlastComponentOutput(
+            hits=items
         )
 
     async def setup_jobs(self):
@@ -49,20 +55,21 @@ class DiffDockComponent(Component[DiffDockComponentInput, DiffDockComponentOutpu
 
         self.jobs = []
 
-        for protein_id in self.input.proteins_with_pdb:
-            for ligand_id in self.input.ligands:
-                protein: Protein = Protein.objects.with_id(protein_id)
-                ligand: Ligand = Ligand.objects.with_id(ligand_id)
+        for protein_id in self.input.proteins_with_fasta:
+            protein: Protein = Protein.objects.with_id(protein_id)
 
-                result = await setup_job_feature.handle(request=SetupJobRequest(
-                    experiment_id=self.experiment.id,
-                    protein_id=protein.iid.value,
-                    ligand_id=ligand.iid.value,
-                    job_id=None,
-                    job_name=f'DiffDock {protein.name.fasta_name}'
-                ))
+            result = await setup_job_feature.handle(request=SetupJobRequest(
+                experiment_id=self.experiment.id,
+                protein_id=protein.iid.value,
+                descriptions=self.input.descriptions,
+                alignments=self.input.alignments,
+                hitlist_size=self.input.hitlist_size,
+                expect=self.input.expect,
+                job_id=None,
+                job_name=f'Blast {protein.name.fasta_name}'
+            ))
 
-                self.jobs.append(BlastJob.objects.with_id(result.job_id))
+            self.jobs.append(BlastJob.objects.with_id(result.job_id))
 
     async def jobs_setup_errors(self) -> List[JobValidationError]:
         jobs_errors = []
@@ -80,9 +87,9 @@ class DiffDockComponent(Component[DiffDockComponentInput, DiffDockComponentOutpu
         return jobs_errors
 
     @property
-    def _input_parameter_type(self) -> Type[DiffDockComponentInput]:
-        return DiffDockComponentInput
+    def _input_parameter_type(self) -> Type[BlastComponentInput]:
+        return BlastComponentInput
 
     @property
-    def _output_parameter_type(self) -> Type[DiffDockComponentOutput]:
-        return DiffDockComponentOutput
+    def _output_parameter_type(self) -> Type[BlastComponentOutput]:
+        return BlastComponentOutput
